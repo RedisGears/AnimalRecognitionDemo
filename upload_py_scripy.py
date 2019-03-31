ad83b5528bfa9f7ec13dfedbf02f27460b19ca40
Meir Shpilraien <meir@redislabs.com>
2019-03-31 07:27:01 +0300

    added password option to upload_py_scripy.py

--- upload_py_scripy.py
+++ upload_py_scripy.py
@@ -12,14 +12,17 @@ parser.add_argument(
     '--port', default=6379, type=int,
     help='redis port')
 
-
 parser.add_argument(
     '--file-path', default=None,
     help='script path')
 
+parser.add_argument(
+    '--password', default=None,
+    help='redis password')
+
 args = parser.parse_args()
 
-r = redis.Redis(args.host, args.port)
+r = redis.Redis(args.host, args.port, password=args.password)
 f = open(args.file_path, 'rt')
 script = f.read()
 res = r.execute_command('rg.pyexecute', script)
